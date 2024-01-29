40776a022ac9e5957d22bf2c49deb81481cacfc9
Yash Rai <yash_rai@thbs.india.com>
2024-01-29 17:15:28 +0530

    yahs

---
+++ tetsing/src/main/java/com/app/TetsingApplication.java
@@ -0,0 +1,14 @@
+package com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TetsingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TetsingApplication.class, args);
+		System.out.println("yash rai");
+	}
+
+}
